22ef999d038360e863de22c6c401518c250fce83
swan28 <swati28.g@gmail.com>
2025-02-09 23:09:39 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="shipment-price-prediction",
+    version='0.0.1',
+    author='swati',
+    author_email='emailid@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
